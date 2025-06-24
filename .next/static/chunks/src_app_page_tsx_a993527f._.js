60827eb572c96a18d2bdbb7806948b8c837dd975
StavoMidnite661 <StavoMidnite@proton.me>
2025-06-23 21:21:20 -0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1750738879958

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_@ethersproject_abi_lib_esm_7492c8f4._.js",
+  "static/chunks/node_modules_@ethersproject_providers_lib_esm_a4d85dc6._.js",
+  "static/chunks/node_modules_d424c896._.js",
+  "static/chunks/src_ad8da3ba._.js"
+],
+    source: "dynamic"
+});
